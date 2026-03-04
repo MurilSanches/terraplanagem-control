fbff5c658c641fe5cccf2554bdfafd144ca3b056
MUrilo Sanches de Paula <murilo.de.paula2011@gmail.com>
2026-03-04 10:57:20 -0300

    ajuste ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,9 +3,11 @@ module.exports = {
     {
       name: 'api',
       script: 'dist/main.js',
+      cwd: '/var/www/app/backend',
       instances: 1,
       autorestart: true,
       watch: false,
+      max_memory_restart: '300M',
       env_production: {
         NODE_ENV: 'production',
       },
